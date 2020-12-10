bbe15c34f79cc7cafd4165fc371b9aad5e20e8d3
César Filho <cesarfilho09.cf@gmail.com>
2020-12-10 14:19:40 -0300

    layout lista de usuários

---
+++ src/components/UserItem/UserItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="d-flex border-bottom pb-2 align-items-center justify-content-between">
+    <div class="d-flex">
+      <img
+        src="https://via.placeholder.com/80"
+        alt=""
+        class="img-fluid rounded-circle"
+      />
+      <div class="pl-3">
+        <h5>
+          César Filho Lima
+        </h5>
+        <p>
+          cesarfilho078@gmail.com
+        </p>
+      </div>
+    </div>
+    <div class="d-flex justify-content-end">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserItem",
+  props: {
+    name: String,
+    email: String,
+    image: String,
+  },
+};
+</script>
--- src/pages/Chat/ChatComponent.vue
+++ src/pages/Chat/ChatComponent.vue
@@ -1,16 +1,33 @@
 <template>
-    <h4>
-        Chart.Js
-    </h4>
+    <section class="section-chat">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="card shadow-sm border-0">
+                        <div class="card-body">
+                            <UserItem />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
+<style>
+    @import './Chat.css';
+</style>
 <script>
 import Auth from '../../Models/Auth';
+import UserItem from '../../components/UserItem/UserItem'
 export default {
     beforeCreate(){
         let isAuth = Auth.verifyAuthenticated();
         if(!isAuth){
             window.location.href = '/login'
         }
+    },
+    components : {
+        'UserItem' : UserItem
     }
 }
 </script>
